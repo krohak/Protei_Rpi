7354b3d2ef89a03783f7e005b5bc00fa7c1578a2
krohak <krohak@hku.hk>
2017-05-14 01:50:24 +0800

    Switched to one channel for updating the MQTT feed. Data includes Device ID as well. Computing MAC for integrity check using secret key

--- send_data.py
+++ send_data.py
@@ -1,63 +1,53 @@
 import paho.mqtt.publish as publish
 import sys
 import datetime
+import hmac
 from sense_hat import SenseHat
 
 sense = SenseHat()
+digest_maker = hmac.new('PASSWORD')
 
 hostname = "iot.eclipse.org" # Sandbox broker
 port = 1883 # Default port for unencrypted MQTT
 
-topic_time = "TIME"
-topic_hum = "HUM"
-topic_temp = "TEMP"
-topic_pres = "PRES"
-topic_nor = "NOR"
+topic_far="TOPIC"
 
 try:
 
-
-
+	device="DEVICE#"
+	coord=[114.12915,22.282231]
         humidity = str(sense.get_humidity())
         print(humidity)
-        publish.single(topic_hum, payload=humidity,
-        	qos=1,
-        	hostname=hostname,
-        	port=port)
-
         temp = str(sense.get_temperature())
         print(temp)
-        publish.single(topic_temp, payload=temp,
-        	qos=1,
-        	hostname=hostname,
-        	port=port)
-
         pressure = str(sense.get_pressure())
         print(pressure)
-        publish.single(topic_pres, payload=pressure,
-        	qos=1,
-        	hostname=hostname,
-        	port=port)
-
         north = str(sense.get_compass())
         print(north)
-        publish.single(topic_nor, payload=north,
-        	qos=1,
-        	hostname=hostname,
-        	port=port)
-
-        date=str(datetime.datetime.now())
+	date=str(datetime.datetime.now())
         print(date)
-        publish.single(topic_time, payload=date,
-        	qos=1,
-        	hostname=hostname,
-        	port=port)
 
+	qstr=('{"coordinates":%s,"properties":{"Device":%s,"Time":"%s","Temperature":%s,"Pressure":%s,"Humidity":%s,"Magnetometer":%s}}'%(coord,device,date,temp,pressure,humidity,north))
 
+	digest_maker.update(qstr)
+	
+	digest = digest_maker.hexdigest()
+	print(digest)
+	
+	to_send=digest+'_'+qstr
+	#to_send="digest"+'_'+qstr
+	
+	publish.single(topic_far, payload=str(to_send),
+                qos=1,
+                hostname=hostname,
+                port=port)
 
+	
+	print(to_send)
 
 
 except Exception,e:
         print("failed")
         print(e)
         sys.exit(1)
+
