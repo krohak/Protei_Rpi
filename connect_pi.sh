7f9e99a13184acd0f3781253b257a010c8b8fc17
krohak <krohak@hku.hk>
2017-05-15 03:48:12 +0800

    Instead of the ESSID of Wi-Fi, shell script now takes arguments in the form of coordinates

--- connect_pi.sh
+++ connect_pi.sh
@@ -1,28 +1,30 @@
+network="1234"
+
 echo "Switching on Wi-Fi"
 #sudo ifconfig wlan0 up
 
-echo "Finding network $1"
-a="`sudo iwlist wlan0 scan | grep $1`"
+echo "Finding network $network"
+a="`sudo iwlist wlan0 scan | grep $network`"
 
 if [ $a ]
 then
 	echo $a
-	echo "Found network $1"
+	echo "Found network $network"
 	echo "Connecting..."
-	#sudo iwconfig wlan0 essid $1 key s:password
+	#sudo iwconfig wlan0 essid $network key s:password
 	#sudo dhclient wlan0
-	echo "Sending Data through $1"
-	python send_data.py 114 22 2
+	echo "Sending Data through $network"
+	python send_data.py $1 $2 2
 	echo "Data Sent"
 	echo "Disconnecting Wi-Fi"
 	#sudo ifconfig wlan0 down
 else
-	echo "Not found network $1"
+	echo "Not found network $network"
 	echo "Disconnecting Wi-Fi"
   #sudo ifconfig wlan0 down
 	echo "Switching on Mobile Data"
 	echo "Sending data to server using Mobile Data"
-	python send_data.py 114 22 1
+	python send_data.py $1 $2 1
 	echo "Data Sent"
 	echo "Disconnecting Mobile Data"
 fi
