66ff7408439cf5479f6dcac17a4f86093139dc60
krohak <rohaksinghal14@gmail.com>
2017-05-15 19:33:07 +0000

    Second client listens to update from the server to set new threshold value for the number of coordinates from which the data needs to be aggregated. Added try-except blocks to prevent the program from unexpectedly haulting

--- broker_pi.py
+++ broker_pi.py
@@ -6,51 +6,76 @@ import hmac
 import json
 
 count=0
+max=3
 
 hostname = "iot.eclipse.org"
-topic_near="TOPIC"
+topic_near="krohak/near"
 port=1883
 
 def on_connect(client, userdata, flags, rc):
   print("Connected with result code "+str(rc))
-  client.subscribe("TOPIC")
+  client.subscribe("CHANNEL1")
+
+def on_connect2(client, userdata, rc):
+  print("Connected with result code2 "+str(rc))
+  client.subscribe("CHANNEL2")
+
 
 def on_message(client, userdata, msg):
-	hash=(msg.payload.decode("utf-8")).split("_")[0]
-	data=(msg.payload.decode("utf-8")).split("_")[1]
-	digest_maker = hmac.new('PASSWORD')
-        digest_maker.update(data)
-	digest = digest_maker.hexdigest()
+	if(msg.topic=="CHANNEL1"):
+		hash=(msg.payload.decode("utf-8")).split("_")[0]
+		print(("Received Hash: %s")%(hash))
+		data=(msg.payload.decode("utf-8")).split("_")[1]
+		digest_maker = hmac.new('PASSWORD')
+        	digest_maker.update(data)
+		digest = digest_maker.hexdigest()
+		print(("Computed Hash: %s")%(digest))
 
 
-	if digest == hash:
-		print("Valid data")
-		with open('log.json', 'a') as f:
-			print(data,file=f)
-		global count
-		count+=1
-		print(count)
+		if digest == hash:
+			print("Valid data")
+			with open('log.json', 'a') as f:
+				print(data,file=f)
+			global count
+			count+=1
+			print(count)
+		else:
+			print("Invalid data")
+
+	
+
+def on_message2(client, userdata, msg):
+	if(msg.topic=="CHANNEL2"):
+		print(("Received new threshold value: %s")%msg.payload.decode("utf-8"))
+		try:
+			global max
+			max=int(msg.payload.decode("utf-8"))
+		except Exception,e:
+			print(e)
 	
     
 client = mqtt.Client()
+client2 = mqtt.Client()
 client.connect("localhost",1883,60)
+client2.connect("iot.eclipse.org",1883)
 
 client.on_connect = on_connect
 client.on_message = on_message
+
+client2.on_connect = on_connect2
+client2.on_message = on_message2
+
 
 #client.loop_start()
 
 while True:
 	client.loop_start()
+	client2.loop_start()
 	time.sleep(2)
-	'''
-	no=0
-	with open('log.json') as f:
-		no=(sum(1 for _ in f))
-		print(no)
-	'''
-	if count>=3:
+	global max
+	if count>=max:
 		client.loop_stop()
+		client2.loop_stop()
 		cord_list=[]
 		avg_temp=0
 		avg_hum=0
@@ -62,9 +87,9 @@ while True:
 			avg_temp+=packet["properties"]["Temperature"]
 			avg_hum+=packet["properties"]["Humidity"]
 			avg_pres+=packet["properties"]["Pressure"]
-		avg_temp/=3
-		avg_hum/=3
-		avg_pres/=3
+		avg_temp/=count
+		avg_hum/=count
+		avg_pres/=count
 		qstr=('{"coordinates":%s,"properties":{"Temperature":%s,"Pressure":%s,"Humidity":%s}}'%(cord_list,avg_temp,avg_pres,avg_hum))
 		print(qstr)
 		digest_maker = hmac.new('PASSWORD')
@@ -72,14 +97,16 @@ while True:
 		digest = digest_maker.hexdigest()
 		
 		to_send=digest+'_'+qstr
-		publish.single(topic_near, payload=str(to_send),
-                qos=1,
-                hostname=hostname,
-                port=port)
-		f.seek(0)
-		f.truncate()
-		f.close()
-		count=0
-	#lastly
-	print("done")
+		try:
+			publish.single(topic_near, payload=str(to_send),
+                	qos=1,
+                	hostname=hostname,
+                	port=port)
+			f.seek(0)
+			f.truncate()
+			f.close()
+			count=0
+			print("Data sent")
+		except Exception,e:
+			print(e)
 	time.sleep(2)
